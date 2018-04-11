e52619a07120ab458299b50ed69b4b8cf188c836
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-04-11 11:47:06 -0600

    Added new client side stuff

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -1,9 +1,18 @@
 class Api::PropertiesController < ApplicationController
- before_action :authenticate_user!
+ before_action :set_page
 
   def index
-    render json: Property.available
+    propertires =Property.page(@page).available
+    total_pages = properties.total_pages
+    render json: {
+      properties: properties,
+      total_apges: total_pages,
+    }
   end
 
-  
+private
+  def set_page
+    @page = params[:page] || 1
+  end
+
 end
